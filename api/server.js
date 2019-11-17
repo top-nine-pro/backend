a28854ff32bdad3a065fface6f0f9e57455013d0
alec blakeley <alecblkly@gmail.com>
2019-11-17 16:49:11 -0500

    Added path for login/register. Fixed index.js to run server (.use to .listen)

--- api/server.js
+++ api/server.js
@@ -3,11 +3,18 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 // User Router/Categories Router for Import, after being built
+const authLogReg = require("../auth/auth-log-reg");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/auth", authLogReg)
+
+server.get("/", (req, res) => {
+    res.send("My Top Nine")
+})
 
 module.exports = server;
--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const server = require("./api/server");
 
 const PORT = process.env.PORT || 8000;
 
-server.use(PORT, () => {
+server.listen(PORT, () => {
   console.log(`\n=== Server is running on port ${PORT}. ===\n`);
 });
