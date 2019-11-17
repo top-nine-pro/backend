41a6d3be6f2f4214ae50d1163d0620f15eb37b3d
alec blakeley <alecblkly@gmail.com>
2019-11-17 16:21:49 -0500

    Jwt Token function created.

--- auth/auth-log-reg.js
+++ auth/auth-log-reg.js
@@ -25,11 +25,28 @@ router.post("/register", (req, res) => {
         res.status(500).json(`There was an error with registration: ${error}.`);
       });
   } else {
-    res
-      .status(400)
-      .json({
-        messsage: "Invalid credentials for account creation.",
-        errors: validateResult.errors
-      });
+    res.status(400).json({
+      messsage: "Invalid credentials for account creation.",
+      errors: validateResult.errors
+    });
   }
 });
+
+// Login User
+
+// jwtToken Request
+function getJwtToken(username) {
+  const payload = {
+    username
+  };
+
+  const secret = process.env.JWT_SECRET || "token safe, token secret";
+
+  const options = {
+    expiresIn: "8h"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+module.exports = router;
