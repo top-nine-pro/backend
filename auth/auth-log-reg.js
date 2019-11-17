b0929e2afbee88087b7218e202a4695c3823937e
alec blakeley <alecblkly@gmail.com>
2019-11-17 16:18:59 -0500

    Created user registration POST.

---
+++ auth/auth-log-reg.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const Users = require("../users/users-model");
+const { validateUser } = require("../users/users-helpers");
+
+// Register User
+router.post("/register", (req, res) => {
+  let user = req.body;
+
+  const validateResult = validateUser(user);
+
+  if (validateResult.isSuccessful === true) {
+    const hash = bcrypt.hashSync(user.password, 12);
+    user.password = hash;
+
+    Users.add(user)
+      .then(result => {
+        res
+          .status(201)
+          .json(`${result.username} has been successfully created.`);
+      })
+      .catch(error => {
+        res.status(500).json(`There was an error with registration: ${error}.`);
+      });
+  } else {
+    res
+      .status(400)
+      .json({
+        messsage: "Invalid credentials for account creation.",
+        errors: validateResult.errors
+      });
+  }
+});
