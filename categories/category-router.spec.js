75c5d55c9c14016fd9ec08c056e34d9ee8b0558e
alec blakeley <alecblkly@gmail.com>
2019-11-20 16:50:42 -0500

    Added test for PUT. Tested success and failure of test.

--- categories/category-router.spec.js
+++ categories/category-router.spec.js
@@ -1,8 +1,6 @@
 const request = require("supertest");
 const server = require("../api/server");
 
-// Need to have test for POST /api/categories
-// Need to have test for PUT /api/categories/:id
 // Need to have test for DELETE /api/categories/:id
 
 describe("category-router", () => {
@@ -20,25 +18,25 @@ describe("category-router", () => {
         done();
       });
   });
-  it("should get a list of categories", async () => {
+  it("should GET a list of categories", async () => {
     await request(server)
       .get("/api/categories")
       .set("Authorization", token)
       .expect(200);
   });
-  it("should get a specific category", async () => {
+  it("should GET a specific category", async () => {
     await request(server)
       .get("/api/categories/1")
       .set("Authorization", token)
       .expect(200);
   });
-  it("should get a list of categories for a user", async () => {
+  it("should GET a list of categories for a user", async () => {
     await request(server)
       .get("/api/categories/1/user")
       .set("Authorization", token)
       .expect(200);
   });
-  it("should post a new category", async () => {
+  it("should POST a new category", async () => {
     await request(server)
       .post("/api/categories")
       .set("Authorization", token)
@@ -49,5 +47,18 @@ describe("category-router", () => {
         user_id: 2
       })
       .expect(201);
+  });
+  it("should PUT a category update", async () => {
+    await request(server)
+      .put("/api/categories/1")
+      .set("Authorization", token)
+      .send({
+        id: 1,
+        name: "Testing - Dogs",
+        imageUrl:
+          "https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg",
+        user_id: 1
+      })
+      .expect(200);
   });
 });
